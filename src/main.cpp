17c74928a4d0a0ae5ec20724f44d4ec2b46c8f79
Omar Al Asaad <omarelassaad93@gmail.com>
2024-04-01 00:09:21 +0300

    HTML Work

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,34 @@
 #include "crow_all.h"
 
-int main() {
-    crow::SimpleApp app; //define your crow application
+int main()
+{
+    crow::SimpleApp app;
 
     //define your endpoint at the root directory
     CROW_ROUTE(app, "/")([](){
-        return "Hello World";
+
+        crow::response response;
+        response.set_static_file_info("./static/style.css");
+        response.set_static_file_info("./static/script.js");
+
+        auto page = crow::mustache::load("index.html");
+        crow::mustache::context ctx;
+
+		std::vector<int> ints = {1, 2, 3, 4};
+		ctx["gpus"] = ints;
+
+		std::vector<std::string> objects = {"flags","something else", "something else"};
+		ctx["object-selection"] = objects;
+
+		std::vector<int> dimensions = {1, 2, 4, 8, 16, 32};
+		ctx["dimension-selection"] = dimensions;
+
+		return page.render(ctx);
     });
 
-    //set the port, set the app to run on multiple threads, and run the app
+
+    // crow::response response;
+    // response.set_static_file_info("./templates/style.css");
+
     app.port(8888).multithreaded().run();
 }
---
+++ src/static/script.js
@@ -0,0 +1,22 @@
+function previousOption() {
+    var dropdown = document.getElementById("gpu_dropdown");
+    var currentIndex = dropdown.selectedIndex;
+    var lastIndex = dropdown.options.length - 1;
+    if (currentIndex === 0) {
+        dropdown.selectedIndex = lastIndex;
+    } else {
+        dropdown.selectedIndex = currentIndex - 1;
+    }
+}
+
+function nextOption() {
+    var dropdown = document.getElementById("gpu_dropdown");
+    var currentIndex = dropdown.selectedIndex;
+    var lastIndex = dropdown.options.length - 1;
+    if (currentIndex === lastIndex) {
+        dropdown.selectedIndex = 0;
+    } else {
+        dropdown.selectedIndex = currentIndex + 1;
+    }
+}
+
